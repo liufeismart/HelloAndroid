385bdba583856d5cbfa471efb6c210bd19df550d
liufeismart@163.com <liufeismart@163.com>
2018-07-27 10:57:30 +0800

    Language Settings

--- app/src/main/java/com/liufeismart/helloandroid/activity/LanguageSettingsActiivty.java
+++ app/src/main/java/com/liufeismart/helloandroid/activity/LanguageSettingsActiivty.java
@@ -1,18 +1,13 @@
 package com.liufeismart.helloandroid.activity;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.TextView;
 
 import com.liufeismart.helloandroid.R;
+import com.liufeismart.helloandroid.utils.LanguageUtil;
 
 import java.util.Locale;
 
@@ -25,8 +20,6 @@ public class LanguageSettingsActiivty extends Activity {
     private TextView tv_chinese;
     private TextView tv_english;
 
-    private final String LANGUAGE = "language";
-    private final String COUNTRY = "country";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,61 +30,23 @@ public class LanguageSettingsActiivty extends Activity {
         tv_chinese.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                changeAppLanguage(LanguageSettingsActiivty.this, Locale.SIMPLIFIED_CHINESE, true);
+                LanguageUtil.changeAppLanguage(LanguageSettingsActiivty.this, Locale.SIMPLIFIED_CHINESE, true);
+//                LanguageUtil.getSystemProp("persist.sys.language", "zh");
+                Locale locale = Locale.CHINESE;
+                LanguageUtil.setLanguage(locale);
             }
         });
         tv_english.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                changeAppLanguage(LanguageSettingsActiivty.this, Locale.ENGLISH, true);
+                LanguageUtil.changeAppLanguage(LanguageSettingsActiivty.this, Locale.ENGLISH, true);
+//                LanguageUtil.getSystemProp("persist.sys.language", "en");
+//                Locale locale = Locale.ENGLISH;
+//                LanguageUtil.setLanguage(locale);
             }
         });
     }
 
 
-    public void changeAppLanguage(Context context, Locale locale,
 
-                                         boolean persistence) {
-
-        Resources resources = context.getResources();
-
-        DisplayMetrics metrics = resources.getDisplayMetrics();
-
-        Configuration configuration = resources.getConfiguration();
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-
-            configuration.setLocale(locale);
-
-        } else {
-
-            configuration.locale = locale;
-
-        }
-
-        resources.updateConfiguration(configuration, metrics);
-
-        if (persistence) {
-
-            saveLanguageSetting(context, locale);
-
-        }
-
-    }
-
-
-
-    private void saveLanguageSetting(Context context, Locale locale) {
-
-        String name = context.getPackageName() + "_" + LANGUAGE;
-
-        SharedPreferences preferences =
-
-                context.getSharedPreferences(name, Context.MODE_PRIVATE);
-
-        preferences.edit().putString(LANGUAGE, locale.getLanguage()).apply();
-
-        preferences.edit().putString(COUNTRY, locale.getCountry()).apply();
-
-    }
 }
---
+++ app/src/main/java/com/liufeismart/helloandroid/utils/LanguageUtil.java
@@ -0,0 +1,112 @@
+package com.liufeismart.helloandroid.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+import android.util.DisplayMetrics;
+
+import java.lang.reflect.Method;
+import java.util.Locale;
+
+/**
+ * Created by humax on 18/7/6
+ */
+
+public class LanguageUtil {
+
+    private static final String LANGUAGE = "language";
+    private static final String COUNTRY = "country";
+
+    /**
+     * 应用内的语言切换
+     * @param context
+     * @param locale
+     * @param persistence
+     */
+    public static void changeAppLanguage(Context context, Locale locale,
+                                         boolean persistence) {
+        Resources resources = context.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        Configuration configuration = resources.getConfiguration();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            configuration.setLocale(locale);
+        } else {
+            configuration.locale = locale;
+        }
+        resources.updateConfiguration(configuration, metrics);
+        if (persistence) {
+            saveLanguageSetting(context, locale);
+        }
+    }
+
+
+
+    private static void saveLanguageSetting(Context context, Locale locale) {
+        String name = context.getPackageName() + "_" + LANGUAGE;
+        SharedPreferences preferences =
+        context.getSharedPreferences(name, Context.MODE_PRIVATE);
+        preferences.edit().putString(LANGUAGE, locale.getLanguage()).apply();
+        preferences.edit().putString(COUNTRY, locale.getCountry()).apply();
+    }
+
+
+    /**
+     *
+     * @param key
+     * @param defaultValue
+     * @return
+     * key 利用adb shell getprop
+     * 不起作用
+     */
+    public static String getSystemProp(final String key, final String defaultValue) {
+        try {
+            Method getStringPropMethod = Class.forName("android.os.SystemProperties")
+                    .getMethod("set", String.class, String.class);
+            return (String) getStringPropMethod.invoke(null, key, defaultValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return defaultValue;
+        }
+    }
+
+
+    public static void setLanguage(Locale locale) {
+        try {
+            Object objIActMag;
+            Class clzIActMag = Class.forName("android.app.IActivityManager");
+            Class clzActMagNative = Class
+                    .forName("android.app.ActivityManagerNative");
+            Method mtdActMagNative$getDefault = clzActMagNative
+                    .getDeclaredMethod("getDefault");
+            objIActMag = mtdActMagNative$getDefault.invoke(clzActMagNative);
+            Method mtdIActMag$getConfiguration = clzIActMag
+                    .getDeclaredMethod("getConfiguration");
+            Configuration config = (Configuration) mtdIActMag$getConfiguration
+                    .invoke(objIActMag);
+            config.locale = locale;
+            Class clzConfig = Class
+                    .forName("android.content.res.Configuration");
+            java.lang.reflect.Field userSetLocale = clzConfig
+                    .getField("userSetLocale");
+            userSetLocale.set(config, true);
+//            Class activityRecordClass = Class
+//                    .forName("com.android.server.am.ActivityRecord");
+            //
+
+            //
+//            Class[] clzParams = { Configuration.class, activityRecordClass, boolean.class, boolean.class };
+//            Method mtdIActMag$updateConfiguration = clzIActMag
+//                    .getDeclaredMethod("updateConfigurationLocked", clzParams);
+//            mtdIActMag$updateConfiguration.invoke(objIActMag, config, null, false, false );
+//            Method mtdIActMag$updateConfiguration = clzIActMag
+//                    .getDeclaredMethod("updateConfigurationLocked");
+//            mtdIActMag$updateConfiguration.invoke(objIActMag);
+//            BackupManager.dataChanged("com.android.providers.settings");
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+}
